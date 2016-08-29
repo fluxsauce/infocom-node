e13821e71a2d386eef01db5b72e06e7cff94ffb1
Jon Peck <jpeck@fluxsauce.com>
2016-08-28 22:00:08 -0700

    Adding Difficulties, but discovering that it's not so DRY.

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const fs = require('fs');
 const parse = require('csv-parse/lib/sync');
 const restify = require('restify');
 
-const Games = require('./lib/games');
 const Genres = require('./lib/genres');
+const Difficulties = require('./lib/difficulties');
+const Games = require('./lib/games');
 
+var difficulties;
 var genres;
 var games;
 
@@ -16,9 +18,10 @@ fs.readFile(`${__dirname}/assets/games.csv`, 'utf8', (err, data) => {
     throw err;
   }
   let games_raw = parse(data, {columns: true});
-  genres = new Genres(games_raw);
 
-  games = new Games(games_raw, genres);
+  genres = new Genres(games_raw);
+  difficulties = new Difficulties(games_raw);
+  games = new Games(games_raw, genres, difficulties);
 
   startServer();
 });
@@ -27,6 +30,8 @@ function startServer() {
   const server = restify.createServer({
     name: 'Infocom'
   });
+  server.get('/difficulties', (req, res, next) => difficulties.respondDifficulties(req, res, next));
+  server.get('/difficulties/:id', (req, res, next) => difficulties.respondDifficultyById(req, res, next));
   server.get('/games', (req, res, next) => games.respondGames(req, res, next));
   server.get('/games/:id', (req, res, next) => games.respondGameById(req, res, next));
   server.get('/genres', (req, res, next) => genres.respondGenres(req, res, next));
