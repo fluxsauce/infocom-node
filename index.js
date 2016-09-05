b2f1767ea513666f3eedb288ccf687c647abfcfc
Jon Peck <jpeck@fluxsauce.com>
2016-09-05 16:59:49 -0700

    Syncing in correct place.

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 'use strict';
 
-const Genres = require('./lib/genres');
-const Difficulties = require('./lib/difficulties');
 const Server = require('./lib/server');
+const models = require('./models');
+const gamesRaw = require('./lib/gamesraw');
+const array = require('lodash/array');
 
-const genres = new Genres();
-const difficulties = new Difficulties();
-const server = new Server(genres, difficulties);
-server.listen();
+models.sequelize.sync({ force: true }).then(() => {
+  // Parse out only Difficulties.
+  const difficulties = array.uniq(gamesRaw.map(
+    (currentValue) => (currentValue.difficulty ? currentValue.difficulty : 'None')));
+
+  difficulties.map(name => models.Difficulty.create({ name }));
+
+  // Parse out only Genres.
+  const genres = array.uniq(gamesRaw.map((currentValue) => currentValue.genre));
+  genres.map(name => models.Genre.create({ name }));
+
+  gamesRaw.map((currentValue) => {
+    const game = models.Game.build({
+      name: currentValue.title,
+      year: currentValue.year,
+      description: currentValue.description,
+    });
+
+    const genreName = currentValue.genre;
+    models.Genre.findAll({
+      where: {
+        name: genreName,
+      },
+    }).spread((genre) => {
+      game.addGenre(genre);
+    });
+
+    const difficultyName = currentValue.difficulty;
+    models.Difficulty.findAll({
+      where: {
+        name: difficultyName !== '' ? difficultyName : 'None',
+      },
+    }).spread((difficulty) => {
+      game.addDifficulty(difficulty);
+    });
+
+    return game.save();
+  });
+
+  const server = new Server();
+  server.listen();
+});
--- models/index.js
+++ models/index.js
@@ -2,8 +2,6 @@
 
 const Sequelize = require('sequelize');
 const path = require('path');
-const gamesRaw = require('../lib/gamesraw');
-const array = require('lodash/array');
 
 const db = {};
 
@@ -55,60 +53,15 @@ db.Game = sequelize.define('game', {
   },
 });
 
-// Parse out only Genres.
-const genres = array.uniq(gamesRaw.map((currentValue) => currentValue.genre));
-
-db.Genre.sync({ force: true })
-  .then(() => genres.map(name => db.Genre.create({ name })));
-
+// Games have Genres.
 db.GameGenre = sequelize.define('game_genre');
 db.Game.belongsToMany(db.Genre, { through: db.GameGenre });
 db.Genre.belongsToMany(db.Game, { through: db.GameGenre });
-db.GameGenre.sync({ force: true });
 
-// Parse out only Difficulties.
-const difficulties = array.uniq(gamesRaw.map(
-  (currentValue) => (currentValue.difficulty ? currentValue.difficulty : 'None')));
-
-db.Difficulty.sync({ force: true })
-  .then(() => difficulties.map(name => db.Difficulty.create({ name })));
-
+// Games have Difficulties.
 db.GameDifficulty = sequelize.define('game_difficulty');
 db.Game.belongsToMany(db.Difficulty, { through: db.GameDifficulty });
 db.Difficulty.belongsToMany(db.Game, { through: db.GameDifficulty });
-db.GameDifficulty.sync({ force: true });
-
-// Populate Games.
-db.Game.sync({ force: true })
-  .then(() => gamesRaw.map((currentValue) => {
-    const game = db.Game.build({
-      name: currentValue.title,
-      year: currentValue.year,
-      description: currentValue.description,
-    });
-
-    const genreName = currentValue.genre;
-    db.Genre.findAll({
-      where: {
-        name: genreName,
-      },
-    }).spread((genre) => {
-      game.addGenre(genre);
-    });
-
-    const difficultyName = currentValue.difficulty;
-    db.Difficulty.findAll({
-      where: {
-        name: difficultyName !== '' ? difficultyName : 'None',
-      },
-    }).spread((difficulty) => {
-      game.addDifficulty(difficulty);
-    });
-
-    game.save();
-
-    return game;
-  }));
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
