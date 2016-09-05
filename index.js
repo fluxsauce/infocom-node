76c8955c4f914e3f10052c529944e919b8623ae7
Jon Peck <jpeck@fluxsauce.com>
2016-09-04 17:48:18 -0700

    Trying to narrow down the problem.

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 'use strict';
 
-const fs = require('fs');
-const parse = require('csv-parse/lib/sync');
+const models = require('./models');
 
 const Genres = require('./lib/genres');
-const Difficulties = require('./lib/difficulties');
-const Games = require('./lib/games');
 const Server = require('./lib/server');
-const Db = require('./lib/db.js');
-
-fs.readFile(`${__dirname}/assets/games.csv`, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
-  const gamesRaw = parse(data, {
-    columns: true,
-  });
-
-  const db = new Db('database.sqlite3');
-
-  const genres = new Genres(gamesRaw, db);
-  const difficulties = new Difficulties(gamesRaw);
-  const games = new Games(gamesRaw, genres, difficulties);
-  const server = new Server(genres, difficulties, games);
-
-  server.listen();
-});
+const genres = new Genres();
+const server = new Server(genres);
+server.listen();
---
+++ models/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+const debug = require('debug')('infocom:models:Genre');
+const path = require('path');
+const gamesRaw = require('../lib/gamesraw');
+const array = require('lodash/array');
+
+const db = {};
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  dialect: 'sqlite',
+  storage: path.join(__dirname, '../database.sqlite3'),
+});
+
+db.Genre = sequelize.define('Genre', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+
+// Parse out only Genres.
+const genres = array.uniq(gamesRaw.map((currentValue) => currentValue.genre));
+debug(genres);
+
+db.Genre.sync({ force: true })
+  .then(() => {
+    Promise.map(genres, (name) => {
+      db.Genre.create({ name });
+    });
+  });
+debug(db.Genre);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
